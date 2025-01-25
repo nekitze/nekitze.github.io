a51ee473ec39b426c79af67cea24ed1fd75ce474
Nikita Zubov <nikita.zubov@outlook.com>
2025-01-26 01:26:16 +0300

    init

---
+++ script.js
@@ -0,0 +1,57 @@
+let offset = 0;
+let limit = 100;
+const tableBody = document.querySelector('#tokenTable tbody');
+const loader = document.createElement('div');
+
+loader.style.cssText = `
+    width: 100%;
+    height: 50px;
+    background-color: #3498db;
+    color: white;
+    text-align: center;
+    line-height: 50px;
+    font-size: 18px;
+`;
+
+function loadMoreTokens() {
+    fetch(`https://tokens.jup.ag/tokens?tags=lst,community&limit=${limit}&offset=${offset}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.length === 0) {
+                loader.remove();
+                return;
+            }
+
+            data.forEach(token => {
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                    <td>
+                        <div class="row-container">
+                            <img src="${token.logoURI}" alt="${token.name} logo" class="token-logo">
+                            <div>${token.name}</div>
+                        </div>
+                    </td>
+                    <td>${token.daily_volume}</td>
+                `;
+                tableBody.appendChild(row);
+            });
+
+            offset+=limit;
+        })
+        .catch(error => console.error('Ошибка:', error));
+}
+
+function handleScroll() {
+    const scrollBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 200;
+
+    if (scrollBottom && !loader.parentNode) {
+        tableBody.appendChild(loader);
+        loadMoreTokens();
+    } else if (!scrollBottom && loader.parentNode) {
+        loader.remove();
+    }
+}
+
+window.addEventListener('scroll', handleScroll);
+
+loadMoreTokens();
